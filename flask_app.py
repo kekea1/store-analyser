e3d8dae574cf2580ce464709675b615c7e4a5d2e
kekea1 <53305663+kekea1@users.noreply.github.com>
2021-12-21 21:24:54 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -69,11 +69,6 @@ def hello_world():
 def home():
     return render_template("home.html")
 
-host="0.0.0.0"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8081,debug=True)
-
 
 
 ####COMPTEUR DE VISITEUR####
